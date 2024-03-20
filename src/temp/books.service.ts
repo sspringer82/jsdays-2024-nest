dbce886d341cd52fa760f55e8e7d92b9b5f83d75
Sebastian Springer <88656+sspringer82@users.noreply.github.com>
2024-03-20 11:34:20 +0100

    types

--- src/temp/books.service.ts
+++ src/temp/books.service.ts
@@ -1,21 +1,32 @@
+export type Book = {
+  id: number;
+  title: string;
+  author: string;
+  price: number;
+  pages: number;
+  year: number;
+};
+
+export type CreateBook = Omit<Book, 'id'>;
+
 export class BooksService {
-  private books = [];
+  private books: Book[] = [];
 
   generatedId(): number {
-    const ids = books.map((book) => book.id);
+    const ids = this.books.map((book) => book.id);
     const highestId = Math.max(...ids);
     return highestId + 1;
   }
 
-  getAll() {
+  getAll(): Book[] {
     return this.books;
   }
 
-  getOne(id: number) {
+  getOne(id: number): Book {
     return this.books.find((book) => book.id === id);
   }
 
-  create(newBook) {
+  create(newBook: CreateBook): Book {
     const id = this.generatedId();
     const book = { id, ...newBook };
     this.books.push(book);
@@ -27,7 +38,7 @@ export class BooksService {
     this.books.splice(index, 1);
   }
 
-  update(id: number, book) {
+  update(id: number, book: Book): Book {
     const index = this.books.findIndex((book) => book.id === id);
     this.books[index] = book;
     return book;
